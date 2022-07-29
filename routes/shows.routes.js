e038f0ef049254733d4f788dbc07686de4b0afe1
Paula Rodríguez <paularb923@hotmail.com>
2022-07-29 14:27:28 +0100

    shows-by-genre page

--- routes/shows.routes.js
+++ routes/shows.routes.js
@@ -1,5 +1,8 @@
+
+
 const router = require("express").Router();
-const {getPopularSeriesService} = require("../services")
+const {getPopularSeriesService  } = require("../services")
+const {getGenreList} = require("../services")
 
 
 router.get("/", async (req, res, next) => {
@@ -10,5 +13,13 @@ router.get("/", async (req, res, next) => {
     res.render("shows/home.hbs", {arrData})
 })
 
+router.get ("/genre/:genreId", async (req, res, next) => {
+ const {genreId} =req.params
+ 
+const genre = await getGenreList(genreId)
+console.log(genre)
+ res.render("shows/shows-by-genre.hbs", {genre})
+
+})
 
 module.exports = router;
--- services/index.js
+++ services/index.js
@@ -8,6 +8,11 @@ function getPopularSeriesService() {
     return service.get(`/3/tv/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`)
 }
 
+function getGenreList(genreId) {
+    return service.get(`/3/discover/movie?api_key=${process.env.API_KEY}&with_genres=${genreId}`)
+}
+
 module.exports = {
-    getPopularSeriesService
+    getPopularSeriesService, 
+    getGenreList
 }
