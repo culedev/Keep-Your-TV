13f119a5fe6e67268a3965a679dd9305402ba081
Ivan Culebra <ivan.culebra.munera@gmail.com>
2022-07-30 12:34:40 +0200

    fav logic

--- routes/shows.routes.js
+++ routes/shows.routes.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-const Show = require("../models/Shows.model")
+const Show = require("../models/Shows.model");
 
 const {
   getPopularShowsService,
   getDetailsShowsService,
   getGenreList,
-  getActors
+  getActors,
 } = require("../services");
 
 // GET "/shows" Homepage popular shows
@@ -25,7 +25,8 @@ router.get("/:apiId/details", async (req, res, next) => {
     const actors = await getActors(apiId);
 
     res.render("shows/details.hbs", {
-      arrData, actors: actors.data.cast.slice(0,5)
+      arrData,
+      actors: actors.data.cast.slice(0, 5),
     });
   } catch (err) {
     next(err);
@@ -35,30 +36,76 @@ router.get("/:apiId/details", async (req, res, next) => {
 // POST "/shows/:apId/details" tomar datos y almacenar en DB BOTON FAVORITO
 router.post("/:showId/details", async (req, res, next) => {
   const { showId } = req.params;
-    
+
   try {
     const showDetails = await getDetailsShowsService(showId);
     const arrData = showDetails.data;
 
-    const isfavquestionmark = await Show.find({apiId: showId})
-    console.log("AQUI TONTO", isfavquestionmark)
+    const showExists = await Show.exists({ apiId: showId });
+    const isShowFav = await Show.findOne({apiId: showId})
 
-    //!ACAAAAAAABALOOOOOOOOOOOOOOOOO
+    if (isShowFav.isFav === true) {
+      await Show.findByIdAndUpdate(isShowFav._id, {isFav: false})
+      res.redirect(`/shows/${showId}/details`)
+      return;
+    }
 
-    await Show.create({
+    if (isShowFav.isFav === false){
+      await Show.findByIdAndUpdate(isShowFav._id, {isFav: true})
+      res.redirect(`/shows/${showId}/details`)
+      return;
+    }
+
+    if(showExists === null) {
+      await Show.create({
         apiId: arrData.id,
         name: arrData.name,
         img: arrData.poster_path,
         isFav: true,
-        user: req.session.user._id
-    })
-    res.redirect("/shows")
-
+        user: req.session.user._id,
+      });
+      res.redirect(`/shows/${showId}/details`);
+    }
+    
   } catch (err) {
     next(err);
   }
 });
 
+// POST "/shows/:showId/list/create"
+router.get("/shows/:showId/details", async (req,res,next) => {
+    const {showId} = req.params
+    const {pending, watching, watched, nostatus} = req.body
+    console.log(showId, req.body)
+
+    try {
+      const showDetails = await getDetailsShowsService(showId);
+      const arrData = showDetails.data;
+      
+      const showExists = await Show.exists({ apiId: showId });
+      const isStatus = await Show.findOne({apiId: showId})
+
+
+
+      
+      // if(showExists === null) {
+      //   await Show.create({
+      //     apiId: arrData.id,
+      //     name: arrData.name,
+      //     img: arrData.poster_path,
+      //     isFav: false,
+      //     status: ,
+      //     user: req.session.user._id,
+      //   });
+      //   res.redirect(`/shows/${showId}/details`);
+      // }
+
+
+      res.redirect(`/shows/${showId}/details`);
+    } catch (err) {
+      next(err)
+    }
+})
 
 
 // GET LIST BY GENRE "/shows/genre/:genreId"
@@ -68,6 +115,5 @@ router.get("/genre/:genreId", async (req, res, next) => {
   const genre = await getGenreList(genreId);
   res.render("shows/shows-by-genre.hbs", { genre: genre.data.results });
 });
-
 
 module.exports = router;
